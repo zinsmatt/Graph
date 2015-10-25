a8ee5071a698a93d40aa8cefadf063fe777f1ad2
Matthieu Zins <zins.matthieu@gmail.com>
2015-10-25 22:57:29 +0100

    ajout de adjacentIn/OutEdges dans Node

--- src/node.cpp
+++ src/node.cpp
@@ -1,5 +1,6 @@
 #include "node.h"
 #include "node2d.h"
+#include "edge.h"
 
 Node::Node() : node2d(new Node2D())
 {
@@ -14,6 +15,13 @@ bool Node::addAdjacentEdge(Edge *edge)
         if(*it == edge)
             return false; //already stored
     adjacentEdges.push_back(edge);
+    if(edge->isOriented())
+    {
+        if(edge->getNode1() == this)
+            adjacentOutEdges.push_back(edge);
+        else if(edge->getNode2() == this)
+            adjacentInEdges.push_back(edge);
+    }
     return true;
 }
 
@@ -23,6 +31,20 @@ bool Node::removeAdjacentEdge(Edge *edge)
     if(pos == adjacentEdges.end())
         return false; //edge was not adjacent to this node
     adjacentEdges.erase(pos);
+    if(edge->isOriented())
+    {
+        if(edge->getNode1() == this)
+        {
+            auto pos2 = std::find(adjacentOutEdges.begin(),adjacentOutEdges.end(),edge);
+            if(pos2 != adjacentOutEdges.end())
+                adjacentOutEdges.erase(pos2);
+        }else if(edge->getNode2() == this)
+        {
+            auto pos2 = std::find(adjacentInEdges.begin(),adjacentInEdges.end(),edge);
+            if(pos2 != adjacentInEdges.end())
+                adjacentInEdges.erase(pos2);
+        }
+    }
     return true;
 }
 
--- src/node.h
+++ src/node.h
@@ -10,19 +10,21 @@ class Node : public Element
 {
 private:
     Node2D* node2d;
-    std::vector<Edge*> adjacentEdges;
+    std::vector<Edge*> adjacentEdges;       //!< vector of pointers to the adjacent edges
+    std::vector<Edge*> adjacentInEdges;     //!< vector of pointers to only incident edges
+    std::vector<Edge*> adjacentOutEdges;    //!< vector of pointers to only out edges
 
 
 public:
     Node();
 
-    /* \brief add an adjacent edge
+    /* \brief add an adjacent edge and put it in adjacent in/out edges if required
      * \param a pointer to the edge
      * \return true if added, false if already stored
      * */
     bool addAdjacentEdge(Edge* edge);
 
-    /* \brief remove an adjacent edge
+    /* \brief remove an adjacent edge and remove it from adjacent in/out edges if required
      * \param a pointer to the edge
      * \return true if removed, false else
      * */
